60f7cdeb9bb31e5e6ab46efe3c29109a1c4a9f40
Paranhas <rodrigoparanhas.br@gmail.com>
2026-04-09 10:05:24 -0300

    Update 02
    Finalizado a Primeira aula com os botões Show e Hide com delay colocados

---
+++ Assets/Scripts/ScreenBase.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using System.Net.Security;
+using DG.Tweening;
+
+
+namespace Screens
+{
+    public enum ScreenType
+    {
+        Panel,
+        Info_Panel,
+        Shop
+    }
+    public class ScreenBase : MonoBehaviour
+    {
+        public ScreenType screenType;
+
+        public List<Transform> lisOfObjects;
+
+        public bool startHided = false;
+
+        [Header("Animation")]
+        public float animationDuration = 0.3f;
+        public float delayBetweenObjects = 0.05f;
+
+        private void Start()
+            {
+                if (startHided)
+                    HideObjects();
+            }
+
+        [Button]
+        protected virtual void Show()
+        {
+            ShowObjects();
+            Debug.Log("Show");
+        }
+        [Button]
+        protected virtual void Hide()
+        {
+            HideObjects();
+            Debug.Log("Hide");
+        }
+        private void ShowObjects()
+        {
+            for( int i=0; i<lisOfObjects.Count; i++)
+            {
+                var obj = lisOfObjects[i];
+                obj.gameObject.SetActive(true);
+                obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweenObjects);
+            }
+        }
+        private void ForceShowObjects()
+        {
+            lisOfObjects.ForEach(i => i.gameObject.SetActive(true)) ;
+        }
+        private void HideObjects()
+        {
+            lisOfObjects.ForEach(i => i.gameObject.SetActive(false));
+        }
+    }
+}   
